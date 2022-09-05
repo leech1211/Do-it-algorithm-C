c39691b06d3943a50468959f67a601d6adf63955
changhoon <leech1211@nate.com>
2022-09-05 17:05:51 +0900

    sort() compare

--- 동빈나 실전 알고리즘/정렬/정렬/sort.cpp
+++ 동빈나 실전 알고리즘/정렬/정렬/sort.cpp
@@ -2,10 +2,18 @@
 #include <algorithm>
 using namespace std;
 
+bool compare(int a, int b)
+{
+	return a < b;
+	//a가 b보다 작을때 우선적으로 정렬을 하겠다
+	//더 작은값이 앞으로 - 오름차순 -
+	//a > b 인경우 false가 반환되어 내림차순
+}
+
 int main()
 {
 	int a[10] = { 9,3,5,4,1,10,8,6,7,2 };
-	sort(a,a+10);	//배열자체의 메모리주소, 정렬하는 마지막 원소의 메모리주소
+	sort(a,a+10,compare);	//배열자체의 메모리주소, 정렬하는 마지막 원소의 메모리주소
 	for (int i = 0; i < 10; i++)
 	{
 		cout << a[i] << ' ';
